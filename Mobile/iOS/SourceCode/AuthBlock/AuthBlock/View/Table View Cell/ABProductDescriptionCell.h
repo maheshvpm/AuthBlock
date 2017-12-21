097e4d907cbcfe28947eaaf945fe0f418d5f9ebd
Mahesh <mahesh.muthusamy@aspiresys.com>
2017-12-21 18:11:31 +0530

    Added verify ptoduct option

--- Mobile/iOS/SourceCode/AuthBlock/AuthBlock/View/Table View Cell/ABProductDescriptionCell.h
+++ Mobile/iOS/SourceCode/AuthBlock/AuthBlock/View/Table View Cell/ABProductDescriptionCell.h
@@ -8,10 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol ABProductVerifyDelegate < NSObject >
+
+- (void)verifyProduct:(NSString *)productId;
+
+@end
+
 @interface ABProductDescriptionCell : UITableViewCell
 
 @property (weak, nonatomic) IBOutlet UILabel *productTitle;
 @property (weak, nonatomic) IBOutlet UILabel *rating;
 @property (weak, nonatomic) IBOutlet UILabel *productPrice;
+@property (weak, nonatomic) IBOutlet UIButton *verifyBtn;
+
+@property (nonatomic, weak) id <ABProductVerifyDelegate> delegate;
 
 @end
