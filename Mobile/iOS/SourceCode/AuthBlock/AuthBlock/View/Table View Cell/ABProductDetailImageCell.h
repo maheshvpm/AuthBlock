45b1f13e95b20352e7cd6215c4954ffbf4ed3ee7
Mahesh <mahesh.muthusamy@aspiresys.com>
2017-12-21 13:11:08 +0530

    UI modifications in PDP page

---
+++ Mobile/iOS/SourceCode/AuthBlock/AuthBlock/View/Table View Cell/ABProductDetailImageCell.h
@@ -0,0 +1,16 @@
+//
+//  ABProductDetailImageCell.h
+//  AuthBlock
+//
+//  Created by Mahesh Muthusamy on 21/12/17.
+//  Copyright © 2017 Logu Subramaniyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABProductDetailImageCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *productImageView;
+@property (weak, nonatomic) IBOutlet UIButton *certifiedBtn;
+
+@end
---
+++ Mobile/iOS/SourceCode/AuthBlock/AuthBlock/View/Table View Cell/ABProductDetailImageCell.m
@@ -0,0 +1,24 @@
+//
+//  ABProductDetailImageCell.m
+//  AuthBlock
+//
+//  Created by Mahesh Muthusamy on 21/12/17.
+//  Copyright © 2017 Logu Subramaniyan. All rights reserved.
+//
+
+#import "ABProductDetailImageCell.h"
+
+@implementation ABProductDetailImageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
